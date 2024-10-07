c38586859ec619a9d7944dce4e8d969a58faa5fb
Gizem Gök <gizem.gok@icredible.com>
2024-10-07 20:55:19 +0300

    post data added

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -13,4 +13,27 @@ export async function getData() {
   }
 }
 
+export async function postData(data: any) {
+  const url = `http://localhost:5269/api/EticaretApi/CreateProduct`; // POST isteği için doğru endpoint'i kullan
+
+  try {
+    const response = await fetch(url, {
+      method: "POST", // HTTP POST metodu
+      headers: {
+        "Content-Type": "application/json", // JSON veri gönderildiğini belirtir
+      },
+      body: JSON.stringify(data), // Gönderilecek veriyi JSON formatına dönüştür
+    });
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json(); // Yanıtı JSON olarak al
+    console.log(json); // Yanıtı göster
+  } catch (error: any) {
+    console.error(error.message); // Hata mesajını göster
+  }
+}
+
 
